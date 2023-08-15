b1ae92b97345515b7960fc66f750d6a6750319e5
강형규 <kin0204@naver.com>
2023-08-15 14:07:33 +0900

    프로젝트 명 변경

---
+++ VideoAppCoreSqlServer/Videos.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL identity(1,1) PRIMARY KEY
+	,Created DateTimeOffset(7) Default(SysDateTimeOffset() AT TIME ZONE 'Korea Standard Time')
+	,Title nvarchar(255) not null -- 제목
+	,Url nvarchar(max) not null -- URL
+	,Name nvarchar(50) null --이름
+	,Company nvarchar(255) null --회사
+)
